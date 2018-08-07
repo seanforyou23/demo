d358f7c9b6e85490f5cdb730ab4b1fd62cbaeeb2
Michael Spaxman <seanforyou23@gmail.com>
2018-08-07 12:09:56 -0400

    add component to illustrate the problem

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { BlockCopyModule } from 'patternfly-ng';
+
 import { AppComponent } from './app.component';
 
 @NgModule({
@@ -8,7 +10,8 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BlockCopyModule
   ],
   providers: [],
   bootstrap: [AppComponent]
